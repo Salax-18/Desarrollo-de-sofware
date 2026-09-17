a17e1a001f6f9ab6fab9695aca89249506a7c02e
Salax-18 <katheduque08@gmail.com>
2026-09-17 12:35:06 -0500

    Delete Challenge 01 directory

--- Challenge 01/src/components/SearchBar.tsx
+++
@@ -1,4 +0,0 @@
-interface Props { value: string; onChange: (value:string)=>void; }
-export default function SearchBar({ value, onChange }: Props) {
-  return <div className="search"><input value={value} onChange={e=>onChange(e.target.value)} placeholder="Buscar por nombre o teléfono..." /></div>;
-}
